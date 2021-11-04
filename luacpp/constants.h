f90290c2a596de3776f938b4a3b18a49507aa166
wimalopaan <wilhelm.wm.meier@googlemail.com>
2021-11-04 16:17:38 +0100

    luacpp: replaced widget table

--- luacpp/constants.h
+++ luacpp/constants.h
@@ -5,12 +5,32 @@
 #define W_Width 3
 #define W_Height 4
 #define W_FontHeight 5
-#define W_FontHSmall 6
-#define W_FontHLarge 7
-#define W_YOffset 8
-#define W_YPOffset 9
+#define W_YOffset 6
+#define W_YPOffset 7
+#define W_FontHSmall 8
+#define W_FontHLarge 9
+#define W_Zone 10
+#define W_Options 11
 
-#define CFG_NAME 1
+#define CFG_Title 1
+#define CFG_FirstColWidth 2
+#define CFG_StateTimeout 3
+#define CFG_ScrollUdId 4
+#define CFG_ScrollLrId 5
+#define CFG ParamaterDialId 6
+#define CFG_PageSwitchId 7
+#define CFG_RemoteId 8
+#define CFG_Encoding 9
+#define CFG_MixerGlobalVariable 10
+#define CFG_PreviousLsId 11
+#define CFG_NextLsId 12
+#define CFG_SelectLsId 13
+#define CFG_Backend 14
+#define CFG_SafeMode_FlightMode 15
+#define CFG_SafeMode_Timeout 16
+#define CFG_SafeMode_LinkDropoutMax 17
+#define CFG_SafeMode_LsNumber 18
+
 
 #define MENU_NAME 1
 
