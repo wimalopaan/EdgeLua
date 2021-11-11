e048982b9ee12805b5f2de4fd2d368f3f9b3256b
wimalopaan <wilhelm.wm.meier@googlemail.com>
2021-11-11 11:06:26 +0100

    next step in removing magic numbers

--- luacpp/constants.h
+++ luacpp/constants.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #define BASE_DIR "/EDGELUA"
+#define SHM_VAR 1
 
 #define W_X 1
 #define W_Y 2
@@ -54,6 +55,16 @@
 #define ITEM_Virtual 7
 #define ITEM_Rects 8
 
+#define OVERLAY_SwitchID 1
+#define OVERLAY_Item 2
+#define OVERLAY_LSMode 3
+#define OVERLAY_LastValue 4
+
+#define SHORTCUT_SwitchID 1
+#define SHORTCUT_Item 2
+#define SHORTCUT_LSMode 3
+#define SHORTCUT_LastValue 4
+
 #define PHEADER_Title 1
 #define PHEADER_Module 2
 #define PHEADER_LinesStart 3
@@ -70,7 +81,41 @@
 #define PLINE_Line 3
 #define PLINE_Rects ITEM_Rects
 
-#define REMOTE_LastValue = 1
-#define REMOTE_Module = 2
-#define REMOTE_Function = 3
-#define REMOTE_State = 4
+#define REMOTE_LastValue  1
+#define REMOTE_Module  2
+#define REMOTE_Function  3
+#define REMOTE_State  4
+
+#define BUTTONS_PREV 1
+#define BUTTONS_NEXT 2
+#define BUTTONS_SELECT 3
+#define BUTTONS_MENUSELECT 4
+#define BUTTONS_SLIDER_UD 5
+#define BUTTONS_SLIDER_LR 6
+
+#define FSMADR_State 1
+#define FSMADR_STATE_Initial 0
+#define FSMADR_STATE_Output 1
+
+#define FSMSWITCH_LastTime 1
+#define FSMSWITCH_State 2
+#define FSMSWITCH_CyclePage 3
+#define FSMSWITCH_CycleRow 4
+
+#define FSMCONF_LastTime 1
+#define FSMCONF_State 2
+#define FSMCONF_Row 3
+
+#define FSMCONF_STATE_Wait 0
+#define FSMCONF_STATE_BCastOff 1
+#define FSMCONF_STATE_SelectOn 2
+#define FSMCONF_STATE_SendValue 3
+#define FSMCONF_STATE_End 4
+
+#define FSMRSSI_State 1
+#define FSMRSSI_LastTime 2
+
+#define FSMRSSI_STATE_SafeMode 0
+#define FSMRSSI_STATE_WaitUp 1
+#define FSMRSSI_STATE_Normal 2
+#define FSMRSSI_STATE_WaitDown 3
--- luacpp/global.h
+++ luacpp/global.h
@@ -2,6 +2,17 @@
 
 #define DEBUG 
 
+#define USE_VALUE_STORAGE
+
+#define USE_TRIM_NAMES // patch
+#define USE_SHM // patch
+#define USE_LS_STICKY // patch
+
+#define LSFUNC_A_EQ_X 1
+#define LSFUNC_A_GT_X 3
+
+#define TRIMS_MODE_DISABLE_TRIM 31
+
 #include "version.h"
 #include "constants.h"
 #include "macros.h"
