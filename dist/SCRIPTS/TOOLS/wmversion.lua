bc4cf7d1b6078cf312f659db6efa380931d751c3
wimalopaan <wilhelm.wm.meier@googlemail.com>
2021-11-17 07:25:01 +0100

    added tool to show version on radio without enough space to show version in menu

---
+++ dist/SCRIPTS/TOOLS/wmversion.lua
@@ -0,0 +1,22 @@
+--
+-- EdgeLUA - EdgeTx / OpenTx Extensions
+-- Copyright (C) 2021 Wilhelm Meier <wilhelm.wm.meier@googlemail.com>
+--
+-- This work is licensed under the Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License.
+-- To view a copy of this license, visit http:
+-- or send a letter to Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
+-- IMPORTANT
+-- Please note that the above license also covers the transfer protocol used and the encoding scheme and
+-- all further principals of tranferring state and other information.
+       
+       
+       
+       
+local function run()
+    lcd.clear();
+    lcd.drawText(10, 10, "Version: " .. "2.01", MIDSIZE);
+    return 0;
+end
+return {
+  run = run
+};
